5603db2c41cf36ebaae6d7596167534a70b9065a
Hoseon <dawntawnclock@gmail.com>
2023-04-13 10:57:06 +0900

    feat: 내 위치 찾기 추가

--- lib/features/map/view/map_screen.dart
+++ lib/features/map/view/map_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:oduck/features/map/view/grid_screen.dart';
+
+import '../../../config/theme.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -10,25 +15,86 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController _mapController;
+  bool _myLocationEnabled = false;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  final LatLng _center = const LatLng(37.5518911, 126.9917937);
+
+  @override
+  void dispose() {
+    _mapController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    final position = await Geolocator.getCurrentPosition();
+    final cameraPosition = CameraPosition(
+      target: LatLng(position.latitude, position.longitude),
+      zoom: 18,
+    );
+    _mapController
+        .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+    setState(() {
+      _myLocationEnabled = true;
+    });
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     _mapController = controller;
   }
 
+  void _onGridTap() async {
+    await showModalBottomSheet(
+      useSafeArea: true,
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
+      builder: (context) => const GridScreen(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Map"),
-      ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 11.0,
-        ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            myLocationEnabled: _myLocationEnabled,
+            myLocationButtonEnabled: false,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 12.0,
+            ),
+          ),
+          Positioned(
+            bottom: 80,
+            right: 10,
+            child: FloatingActionButton(
+              onPressed: _onGridTap,
+              foregroundColor: primaryColor,
+              backgroundColor: Colors.white,
+              elevation: 8,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10), // 버튼 모서리 둥글기
+              ), // 그림자 크기
+              child: const Icon(Ionicons.grid),
+            ),
+          ),
+          Positioned(
+            bottom: 16,
+            right: 10,
+            child: FloatingActionButton(
+              onPressed: _getCurrentLocation,
+              foregroundColor: primaryColor,
+              backgroundColor: Colors.white,
+              elevation: 8,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10), // 버튼 모서리 둥글기
+              ), // 그림자 크기
+              child: const Icon(Ionicons.location),
+            ),
+          ),
+        ],
       ),
     );
   }

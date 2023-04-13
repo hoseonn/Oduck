8f245f23081af7b47484737db6b06f1b0230f43e
Hoseon <dawntawnclock@gmail.com>
2023-04-13 10:55:53 +0900

    feat: 로그인 페이지 구현중

---
+++ lib/features/authentication/view/login_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 40,
+          ),
+          child: Column(
+            children: [
+              const Text(
+                "오덕에 어서오세요!",
+                // style: TextStyle(color: ),
+              ),
+              Container(
+                height: 70,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/googleLogo.png',
+                      height: 40,
+                      width: 40,
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    const Text(
+                      "구글로 시작하기",
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

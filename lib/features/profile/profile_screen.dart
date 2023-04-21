b44699fb38c2d9485affdea816ecf5a7b2c94af9
Hoseon <dawntawnclock@gmail.com>
2023-04-21 11:50:18 +0900

    feat: 로그아웃 구현

--- lib/features/profile/profile_screen.dart
+++ lib/features/profile/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:oduck/features/profile/view_model/darkMode_view_model.dart';
@@ -20,6 +21,29 @@ class ProfileScreen extends ConsumerWidget {
                 ref.read(darkModeProvider.notifier).setDarkMode(value),
               },
               title: const Text("다크모드"),
+            ),
+            ListTile(
+              title: const Text("로그아웃 하기"),
+              textColor: Colors.red,
+              onTap: () {
+                showCupertinoDialog(
+                  context: context,
+                  builder: (context) => CupertinoAlertDialog(
+                    title: const Text("정말 로그아웃 하시겠습니까?"),
+                    actions: [
+                      CupertinoDialogAction(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: const Text("아니요"),
+                      ),
+                      CupertinoDialogAction(
+                        onPressed: () {},
+                        isDestructiveAction: true,
+                        child: const Text("네"),
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
           ],
         ),

afad808494d536202193db86069f1a5f35f34791
Hoseon <dawntawnclock@gmail.com>
2023-05-03 09:40:09 +0900

    fix: 테마 수정

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -1,35 +1,24 @@
 import 'package:flutter/material.dart';
 
-/// Colors from Tailwind CSS (v3.0) - June 2022
-///
-/// https://tailwindcss.com/docs/customizing-colors
-
 const Color primaryColor = Color(0xFFB58BC1);
 
-const int _textColor = 0xFF9CA3AF;
-const MaterialColor textSwatch = MaterialColor(_textColor, <int, Color>{
-  50: Color(0xFFFFFFFF),
-  100: Color(0xFFF7F9FA),
-  200: Color(0xFFF1F4F6),
-  300: Color(0xFFDADEE5),
-  400: Color(0xFFC2C6CE),
-  500: Color(_textColor),
-  600: Color(0xFF787E89),
-  700: Color(0xFF5A616B),
-  800: Color(0xFF363C45),
-  900: Color(0xFF141C21),
-});
+ThemeData whiteTheme = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: primaryColor,
+  primarySwatch: Colors.pink,
+  appBarTheme: const AppBarTheme(
+    iconTheme: IconThemeData(
+      color: Colors.black,
+    ),
+    titleTextStyle: TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+      fontSize: 18,
+    ),
+  ),
+);
 
-const Color grey000 = Color(0xFFFFFFFF);
-const Color grey100 = Color(0xFFF7F9FA);
-const Color grey200 = Color(0xFFF1F4F6);
-const Color grey300 = Color(0xFFDADEE5);
-const Color grey400 = Color(0xFFC2C6CE);
-const Color grey500 = Color(0xFF9CA3AF);
-const Color grey600 = Color(0xFF787E89);
-const Color grey700 = Color(0xFF5A616B);
-const Color grey800 = Color(0xFF363C45);
-const Color grey900 = Color(0xFF141C21);
-
-const Color errorColor = Color(0xFFDC2626); // red-600
-
+ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  primaryColor: primaryColor,
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:oduck/common/bottom_nav_bar.dart';
+import 'package:oduck/config/theme.dart';
 import 'package:oduck/features/profile/repo/darkmode_repo.dart';
 import 'package:oduck/features/profile/view_model/darkMode_view_model.dart';
+import 'package:oduck/firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 /// Try using const constructors as much as possible!
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   final status = await Geolocator.checkPermission();
   if (status == LocationPermission.denied) {
@@ -43,20 +48,8 @@ class MyApp extends ConsumerWidget {
 
     return MaterialApp(
       title: 'Oduck',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        appBarTheme: const AppBarTheme(
-          iconTheme: IconThemeData(
-            color: Colors.black,
-          ),
-          titleTextStyle: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-            fontSize: 18,
-          ),
-        ),
-      ),
-      darkTheme: ThemeData.dark(),
+      theme: whiteTheme,
+      darkTheme: darkTheme,
       themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
       debugShowCheckedModeBanner: false,
       home: const MainNavigationScreen(
